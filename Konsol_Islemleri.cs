f750649e9ab27ddacc8b7c808d2021e0236e33cc
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-15 19:00:01 +0300

    KonsoldanSayılarıAl adlı konsoldan girilen sayıları önce aralarındaki boşluklara göre bölüp string diziye atayan ve sonra bu elemanları teker teker int değişken yapıp int diziye depolayıp döndüren metodu tamamladım.

--- Konsol_Islemleri.cs
+++ Konsol_Islemleri.cs
@@ -1,3 +1,4 @@
+using System;
 namespace Integer_Ikılilerin_Toplamı
 {
     public class Konsol_Islemleri
@@ -8,6 +9,18 @@ namespace Integer_Ikılilerin_Toplamı
             "Eğer ikililer birbirinden farklıysa toplamları, aynıysa toplamlarının karesi ekrana yazılacak.");
             System.Console.WriteLine("Dipnot: İkili olması için n çift sayı olmalıdır.");
         }
-        
+        public int[] KonsoldanSayılarıAl()
+        {
+            string input = Console.ReadLine();
+            string[] stringSayılar= input.Split(" "); //her sayının arasında konsoldan alırken boşluklara göre bölüp diziye atıyorum.
+            int[] sayılar = new int[stringSayılar.Length]; // sayıları int dizide depolayacağım için uzunluğunu konsoldan alınan sayı kadar belirliyorum.
+            int i=0;
+            foreach (var item in stringSayılar) // string dizideki her sayıyı int değişken yapıp int diziye atıyorum.
+            {
+                sayılar[i]=int.Parse(item);
+                i++;
+            }
+            return sayılar;
+        }
     }
 }
