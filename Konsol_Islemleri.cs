1cc8760567444a4b3ab621874433d5de5f34de29
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-15 18:49:27 +0300

    IlkMesaj adlı metot eklendi.

--- Konsol_Islemleri.cs
+++ Konsol_Islemleri.cs
@@ -2,6 +2,12 @@ namespace Integer_Ikılilerin_Toplamı
 {
     public class Konsol_Islemleri
     {
+        public void IlkMesaj()
+        {
+            System.Console.WriteLine("Lütfen n tane aralarında boşluk olacak şekilde sayı giriniz."+ 
+            "Eğer ikililer birbirinden farklıysa toplamları, aynıysa toplamlarının karesi ekrana yazılacak.");
+            System.Console.WriteLine("Dipnot: İkili olması için n çift sayı olmalıdır.");
+        }
         
     }
 }
