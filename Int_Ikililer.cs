a09be37b564bad36a08f303c1df3d8a2437317c5
Furkan Cengiz <muhammedcengiz1@gmail.com>
2022-02-15 19:20:26 +0300

    Ikıli islemleri yapıp double List döndüren Int_Ikililer metodu oluşturuldu.

--- Int_Ikililer.cs
+++ Int_Ikililer.cs
@@ -1,7 +1,28 @@
+using System;
+using System.Collections.Generic;
 namespace Integer_Ikılilerin_Toplamı
 {
     public class Int_Ikililer
     {
-        
+        public List<double> Ikililerin_Islemleri(int[] arr)
+        {
+            List<double> sonuc = new List<double>();
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                if(arr[i] == arr[i+1])
+                {
+                    double result = Math.Pow(arr[i]+arr[i+1],2);
+                    sonuc.Add(result);
+                }
+                else
+                {
+                    double result = arr[i]+arr[i+1];
+                    sonuc.Add(result);
+                }
+                i++;
+            }
+            return sonuc;
+        }
     }
 }
+// 0-1, 2-3
